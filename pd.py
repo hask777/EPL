3e8178f5428a4961953147635961a8eb3d77fb91
hask777 <haskellisp@gmail.com>
2022-11-27 13:50:38 +0300

    pd

--- pd.py
+++ pd.py
@@ -2,51 +2,73 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
+import time
 # from main import *
 
-'''START FIRST PART'''
-
-standing_urls = "https://fbref.com/en/comps/9/Premier-League-Stats"
-data = requests.get(standing_urls)
-
-soup = BeautifulSoup(data.text)
-standing_table = soup.select('table.stats_table')[0]
-links = standing_table.find_all('a')
+standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
+data = requests.get(standings_url)
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(data.text, features="lxml")
+standings_table = soup.select('table.stats_table')[0]
+links = standings_table.find_all('a')
 links = [l.get("href") for l in links]
 links = [l for l in links if '/squads/' in l]
-# grab all teams main stat links
 team_urls = [f"https://fbref.com{l}" for l in links]
-# the first team link
-team_url = team_urls[0]
-# get html of the team link
-data = requests.get(team_url)
-# print(data.text)
+data = requests.get(team_urls[0])
 
-'''MATCHES IS FIRST DATAFRAME'''
-# Pandas method to parse HTML and create dataframe / Grab Score & Fixtures table
 matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
 # print(matches[0])
 
 # Get Shooting stats table jf the team page
-soup = BeautifulSoup(data.text)
+soup = BeautifulSoup(data.text, features="lxml")
 links = soup.find_all('a')
 links = [l.get("href") for l in links]
-links = [l for l in links if l and 'all_comps/shooting' in l]
-
-'''SHOOTING IS SECOND DATAFRAME'''
-# get url and html jf shooting page
+links = [l for l in links if l and 'all_comps/shooting/' in l]
 data = requests.get(f"https://fbref.com{links[0]}")
 shooting = pd.read_html(data.text, match="Shooting")[0]
 # drop first level (columns names)
 shooting.columns = shooting.columns.droplevel()
-
-'''MERGE MATCHES AND SHOOTINGS DATAFRAMES by DATE column'''
-# from shootings table get date, shoot, shoot on target, distance travel by shoot, freekicks, penaltykicks, penaltykiks attemted
 team_data = matches.merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")
-print(team_data)
 
-'''END OF FIRST PART '''
-# in first part we get info about team matches season & combined with team statistics
+years = list(range(2022, 2020, -1))
+all_matches = []
+standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 
-'''START SECOND PART'''
-# Scraping data for multiple season and teams with a loop
+for year in years:
+    data = requests.get(standings_url)
+    soup = BeautifulSoup(data.text, features="lxml")
+    standings_table = soup.select('table.stats_table')[0]
+
+    links = [l.get("href") for l in standings_table.find_all('a')]
+    links = [l for l in links if '/squads/' in l]
+    team_urls = [f"https://fbref.com{l}" for l in links]
+    
+    previous_season = soup.select("a.prev")[0].get("href")
+    standings_url = f"https://fbref.com{previous_season}"
+    
+    for team_url in team_urls:
+        team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+        data = requests.get(team_url)
+        matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
+        soup = BeautifulSoup(data.text, features="lxml")
+        links = [l.get("href") for l in soup.find_all('a')]
+        links = [l for l in links if l and 'all_comps/shooting/' in l]
+        data = requests.get(f"https://fbref.com{links[0]}")
+        shooting = pd.read_html(data.text, match="Shooting")[0]
+        shooting.columns = shooting.columns.droplevel()
+        try:
+            team_data = matches.merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")
+        except ValueError:
+            continue
+        team_data = team_data[team_data["Comp"] == "Premier League"]
+        
+        team_data["Season"] = year
+        team_data["Team"] = team_name
+        all_matches.append(team_data)
+        time.sleep(1)
+
+match_df = pd.concat(all_matches)
+match_df.columns = [c.lower() for c in match_df.columns]
+
+match_df.to_csv("matches.csv")
+
